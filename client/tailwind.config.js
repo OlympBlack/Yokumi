46f4ce49bd44a39dcd8053da678c0c7951c038dc
Thérance MOUTOUAMA <nabilmoutouama@gmail.com>
2025-06-12 00:14:18 +0100

    feat: Setup du Frontend --------------- Details ---------------
    1- J'ai Ajouter les dépendances suivantes:
    - Tailwind CSS
    - React Router
    - Montserrat
    
    2- J'ai retirer certains  fichiers de bases, et fait une modification, dans le index pour enlever le logo de 'Vite'
    
    3- J'ai ajouter les différents Images, et SVG exportées depuis la maquette

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,jsx, js}"],
+  theme: {
+    extend: {
+      colors:{
+        beige: "#EFEFDB",
+        marron: "#40150D",
+      }
+    },
+  },
+  plugins: [],
+}
+
